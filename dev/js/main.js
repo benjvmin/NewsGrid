54a6aae74366efc69becbfa48f44601608467866
Benjamin Taylor <benjvmintaylor@gmail.com>
2018-02-01 04:10:22 -0500

    Added Homemade Material Click buttons to the News Theme selector

--- dev/js/main.js
+++ dev/js/main.js
@@ -11,8 +11,8 @@ import { Events } from "./modules/PubSub";
       menuGrid: document.querySelector(".menu-grid"),
       menuProperties: [...document.querySelectorAll(".menu-grid__property")],
       SVG: {
-        button: document.querySelector('.dark-mode-toggle'),
-        toggle: document.querySelector('.toggle-button')
+        button: document.querySelector(".dark-mode-toggle"),
+        toggle: document.querySelector(".toggle-button")
       }
     },
     bindEvents() {
@@ -30,7 +30,7 @@ import { Events } from "./modules/PubSub";
       this.elements.button.addEventListener("click", function() {
         Events.emit("ToggleMenu");
       });
-      this.elements.SVG.button.addEventListener("click", function(){
+      this.elements.SVG.button.addEventListener("click", function() {
         Events.emit("ToggleDarkMode");
       });
 
@@ -58,30 +58,33 @@ import { Events } from "./modules/PubSub";
       if (color === undefined) {
         return;
       } else {
-        this.elements.menuProperties.forEach((property) => {
-          if(property.classList.contains('default')) {
-            property.classList.remove('default');
+        this.elements.menuProperties.forEach(property => {
+          if (property.classList.contains("default")) {
+            property.classList.remove("default");
           }
         });
-        if (!item.classList.contains('default')) {
+        if (!item.classList.contains("default")) {
           item.classList.add("default");
         }
-        this.elements.root.style.setProperty("--linear-gradient", `linear-gradient(to bottom, var(--color-one), var(--${color}))`);
+        this.elements.root.style.setProperty(
+          "--linear-gradient",
+          `linear-gradient(to bottom, var(--color-one), var(--${color}))`
+        );
       }
     },
 
     toggleDarkMode() {
       // If DarkMode Enabled
-      if(this.elements.SVG.toggle.classList.contains("dark-active")) {
+      if (this.elements.SVG.toggle.classList.contains("dark-active")) {
         this.elements.SVG.toggle.classList.remove("dark-active");
 
-        //Activate Light Active 
+        //Activate Light Active
         this.elements.SVG.toggle.classList.add("light-active");
         if (this.elements.SVG.toggle.classList.contains("light-active")) {
           this.elements.root.style.setProperty("--menu-color", "rgba(247, 247, 247, 1)");
           this.elements.root.style.setProperty("--font-color", "rgba(37, 41, 46, 0.98)");
         }
-      } else if(this.elements.SVG.toggle.classList.contains("light-active")) {
+      } else if (this.elements.SVG.toggle.classList.contains("light-active")) {
         this.elements.SVG.toggle.classList.remove("light-active");
         this.elements.SVG.toggle.classList.add("dark-active");
 
@@ -93,10 +96,33 @@ import { Events } from "./modules/PubSub";
     },
 
     materialClick(e) {
-      console.log(e.target.getBoundingClientRect());
-      let top = e.target.getBoundingClientRect().top;
-      let left = e.target.getBoundingClientRect().left;
-      console.log((e.clientX - left), (e.clientY - top), top, left);
+      // console.log(e.target.getBoundingClientRect());
+      let top = e.clientY - e.target.getBoundingClientRect().top;
+      let left = e.clientX - e.target.getBoundingClientRect().left;
+      let target = e.target;
+      let counter = 1;
+      let materialCircle = document.createElement("div");
+      materialCircle.addEventListener('animationend', removeMaterial);
+
+      function insertCoordinates() {
+        materialCircle.style.top = `${top}px`;
+        materialCircle.style.left = `${left}px`;
+      }
+
+      function removeMaterial() {
+        if (target.contains(materialCircle)) {
+          materialCircle.remove();
+        }
+        console.log("Material Removed");
+      }
+
+      function expand() {
+        materialCircle.classList.add("material-circle");
+        target.appendChild(materialCircle);
+        insertCoordinates();
+        materialCircle.classList.add("expand");
+      }
+      expand();
     }
   };
 
